e84b5c39761d1c59b05e07641fd43ac0f6e15e0d
Ândreo <andreo.dias.barros@gmail.com>
2017-04-02 16:30:11 -0300

    Adding basic controls

---
+++ Assets/Scripts/Controls.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controls : MonoBehaviour {
+
+    public Rigidbody2D sonic;
+    public float movespeed;
+    public int mvinDirection;
+
+    public void Direction()
+    {
+        if (sonic.velocity == new Vector2(-movespeed, sonic.velocity.y))
+        {
+            mvinDirection = -1;
+
+        }
+        else if (sonic.velocity == new Vector2(movespeed, sonic.velocity.y))
+        {
+            mvinDirection = 1;
+        }
+        else
+        {
+            mvinDirection = 0;
+        }
+    }
+
+
+    // Use this for initialization
+    void Start () {
+        sonic = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Direction();
+		if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            sonic.velocity = new Vector2(-movespeed, sonic.velocity.y);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            sonic.velocity = new Vector2(movespeed, sonic.velocity.y);
+        }
+
+
+    }
+
+}
